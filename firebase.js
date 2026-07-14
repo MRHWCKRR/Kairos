a07a157486b6722e4805a97a36241df8ecc90431
MRHWCKRR <koolkid120809@outlook.com>
2026-07-14 20:14:54 +1000

    Fixed Bug. I checked console turns out I added the code at the bottom of firebase.js twice lol

--- firebase.js
+++ firebase.js
@@ -18,7 +18,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
