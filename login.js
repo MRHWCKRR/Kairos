071e99909cd71d8c75e69d919995af6166c0496d
MRHWCKRR <koolkid120809@outlook.com>
2026-07-14 20:54:43 +1000

    Fixed up typos in login.js There were so many and i didn't check if it worked lol omd 🙏🙏😭😭

--- login.js
+++ login.js
@@ -2,23 +2,29 @@ import { auth } from './firebase.js';
 import {
     signInWithEmailAndPassword,
     setPersistence,
-    broswerLocalPersistence,
-    browserSessionPersistence
+    browserLocalPersistence,
+    browserSessionPersistence,
+    GoogleAuthProvider,  
+    signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-loginForm.addEventListener('submit', async (e) => {
-    e.preventDefult();
 
-    const email = document.getElementById('email').ariaValueMax;
-    const password = document.getElementById('password').ariaValueMax;
-    const rememberMe = document.getElementById('remember-me').ariaChecked;
+const loginForm = document.getElementById('email-login-form');
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const rememberMe = document.getElementById('remember-me').checked;
     const btn = loginForm.querySelector('.login-btn');
 
     btn.textContent = "Signing in...";
     btn.disabled = true;
 
     try {
-        await setPersistence(auth, rememberMe ? broswerLocalPersistence : browserSessionPersistence);
+        await setPersistence(auth, rememberMe ? browserLocalPersistence : browserSessionPersistence);
 
         await signInWithEmailAndPassword(auth, email, password);
         window.location.href = "index.html";
@@ -29,14 +35,12 @@ loginForm.addEventListener('submit', async (e) => {
     }
 });
 
-// Implementation for Google Sign In
-const googleProvider = new GoogleAuthProvider();
 const googleBtn = document.getElementById('btn-google');
 
 googleBtn.addEventListener('click', async () => {
     try {
+        const googleProvider = new GoogleAuthProvider();
         const result = await signInWithPopup(auth, googleProvider);
-        // Successful login
         window.location.href = "index.html";
     } catch (error) {
         console.error("Google Auth Error:", error.message);
